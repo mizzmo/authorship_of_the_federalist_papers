898684ec0a758e4716e926cc74c922502b1c98ee
Toby Surtees <tobysurtees@gmail.com>
2025-10-01 17:54:17 +0100

    Add note to file

---
+++ src/lexical_analysis.py
@@ -0,0 +1,5 @@
+# Measuring For:
+# Average number of words per sentence
+# Sentence Length and Variation
+# Lexical Diversity
+# Can test for only Function words and All Words to see if there is a difference.
