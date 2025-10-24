ea44d418e5861b9e54cbe9f535a9bfff5e2b802d
Toby Surtees <tobysurtees@gmail.com>
2025-10-24 20:26:48 +0100

    Comments for future development of average words per sentence

--- src/lexical_analysis.py
+++ src/lexical_analysis.py
@@ -8,6 +8,20 @@ from process_fed import get_federalist_dict
 
 def average_words():
     federalist_dict, author_dict = get_federalist_dict()
-    print(author_dict)
+    # Get each article.
+    for article_number, article in federalist_dict.items():
+    # Split the article into sentences.
+        print(article_number)
+        print(article)
+    # Calculate the average number of each sentence.
     
+    # Produce metrics for each sentence
+    
+    # Corelate with names of Authors.
+    
+    # Produce separate results for each of the disputed papers
+    
+    # Overlay the results with the average for each author.    
+    
+# Graph the results by name
 average_words()
