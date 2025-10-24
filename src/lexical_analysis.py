4364bded9e90688eaabb5c90919604c4d42f7a6e
Toby Surtees <tobysurtees@gmail.com>
2025-10-24 20:39:44 +0100

    Start work on Word Average per Sentence

--- src/lexical_analysis.py
+++ src/lexical_analysis.py
@@ -3,6 +3,11 @@
 # Sentence Length and Variation
 # Lexical Diversity
 # Can test for only Function words and All Words to see if there is a difference.
+import nltk
+from nltk import tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+nltk.download('punkt_tab')
 
 from process_fed import get_federalist_dict
 
@@ -10,9 +15,15 @@ def average_words():
     federalist_dict, author_dict = get_federalist_dict()
     # Get each article.
     for article_number, article in federalist_dict.items():
-    # Split the article into sentences.
-        print(article_number)
-        print(article)
+        # Split the article into sentences.
+        all_sentences = sent_tokenize(article)
+        # Break down each sentence into words, remove punctuation.
+        for sentence in all_sentences:
+            words = word_tokenize(sentence)
+            print(words)
+            # NOTE Do something to disregard the first couple sentences as they are not part of the original papers / are repetitive.
+        break
+            
     # Calculate the average number of each sentence.
     
     # Produce metrics for each sentence
