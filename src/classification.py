5c85dc341cf978a942906013bc0fbb894aec77dc
Toby Surtees <tobysurtees@gmail.com>
2026-04-21 16:25:48 +0100

    Separation of concerns, split classification into separate file, split into functions.

---
+++ src/classification.py
@@ -0,0 +1,102 @@
+import nltk
+nltk.download('punkt')
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from feature_extraction import simple_feature_extraction
+
+def get_train_test(cleaned_articles, author_dict):
+    # Split the input data into training and test sets
+    # Disputed articles should be used as test sets.
+
+    # Training Set (Non Disputed)
+    train_texts = []
+    train_labels = []
+    for num, article in cleaned_articles.items():
+        if author_dict[num] != "DISPUTED":
+            train_texts.append(article)
+            train_labels.append(author_dict[num])
+
+    # Test Sets (Disputed)
+    test_texts = []
+    test_ids = []
+
+    for num, article in cleaned_articles.items():
+        if author_dict[num] == "DISPUTED":
+            test_texts.append(article)
+            test_ids.append(num)
+            
+    return train_texts, train_labels, test_texts, test_ids
+
+
+def multinomial_cl(cleaned_articles, author_dict):
+    
+
+    train_texts, train_labels, test_texts, test_ids = get_train_test(cleaned_articles, author_dict)
+    
+    # Count Vectorization - Create a Document Term Matrix by counting how many times a word from the vocab appears in a document.
+    vectorizer = CountVectorizer(analyzer='char', ngram_range=(3,5))
+
+    X_train = vectorizer.fit_transform(train_texts)
+    X_test = vectorizer.transform(test_texts)
+    print('\nMultinomialNB')
+    # Multinomial Naive Bayes Classifier
+    mnb = MultinomialNB()
+    mnb.fit(X_train, train_labels)
+
+    predictions = mnb.predict(X_test)
+
+    for i in range(len(test_ids)):
+        print(f"Paper {test_ids[i]} predicted author: {predictions[i]}")
+        
+    #Validation
+    X = vectorizer.fit_transform(train_texts)
+    y = train_labels
+
+    X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size=0.2, stratify=y)
+
+    mnb.fit(X_tr, y_tr)
+    print("Validation accuracy:", mnb.score(X_val, y_val))
+
+
+def linear_cl(cleaned_articles, author_dict):
+    
+    print('\nLinearSVC')
+
+    train_texts, train_labels, test_texts, test_ids = get_train_test(cleaned_articles, author_dict)
+
+    vectorizer = TfidfVectorizer(
+        analyzer='char',
+        ngram_range=(3,5),
+        max_features=5000
+    )
+
+    X_train = vectorizer.fit_transform(train_texts)
+    X_test = vectorizer.transform(test_texts)
+
+    model = LinearSVC(class_weight='balanced')
+    model.fit(X_train, train_labels)
+
+    predictions = model.predict(X_test)
+
+    for i in range(len(test_ids)):
+        print(f"Paper {test_ids[i]} predicted author: {predictions[i]}")
+
+    #Validation
+    X = vectorizer.fit_transform(train_texts)
+    y = train_labels
+
+    X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size=0.2, stratify=y)
+
+    model.fit(X_tr, y_tr)
+    print("Validation accuracy:", model.score(X_val, y_val))
+    
+    
+    
+cleaned_articles, author_dict = simple_feature_extraction()
+#multinomial_cl(cleaned_articles, author_dict)
+linear_cl(cleaned_articles, author_dict)
--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,100 +1,22 @@
 import pandas as pd
 from process_fed import get_federalist_dict
 import re
-import nltk
-nltk.download('punkt')
-
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import LinearSVC
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-cleaned_articles = {}
-federalist_articles, author_dict = get_federalist_dict()
+def simple_feature_extraction():
+    cleaned_articles = {}
+    federalist_articles, author_dict = get_federalist_dict()
 
-# Remove non alphabet characters, all lowercase
-for num, article in federalist_articles.items():  
-    cleaned_articles[num] = (re.sub('[^A-Za-z]', ' ', article)).lower()
-
-# Split the input data into training and test sets
-# Disputed articles should be used as test sets.
-
-# Training Set (Non Disputed)
-train_texts = []
-train_labels = []
-for num, article in cleaned_articles.items():
-    if author_dict[num] != "DISPUTED":
-        train_texts.append(article)
-        train_labels.append(author_dict[num])
-
-# Test Sets (Disputed)
-test_texts = []
-test_ids = []
-
-for num, article in cleaned_articles.items():
-    if author_dict[num] == "DISPUTED":
-        test_texts.append(article)
-        test_ids.append(num)
-
-# Count Vectorization - Create a Document Term Matrix by counting how many times a word from the vocab appears in a document.
-vectorizer = CountVectorizer(analyzer='char', ngram_range=(3,5))
-
-X_train = vectorizer.fit_transform(train_texts)
-X_test = vectorizer.transform(test_texts)
-print('\MultinomialNB')
-# Multinomial Naive Bayes Classifier
-mnb = MultinomialNB()
-mnb.fit(X_train, train_labels)
-
-predictions = mnb.predict(X_test)
-
-for i in range(len(test_ids)):
-    print(f"Paper {test_ids[i]} predicted author: {predictions[i]}")
+    # Remove non alphabet characters, all lowercase
+    for num, article in federalist_articles.items():  
+        cleaned_articles[num] = (re.sub('[^A-Za-z]', ' ', article)).lower()
     
-#Validation
-X = vectorizer.fit_transform(train_texts)
-y = train_labels
-
-X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size=0.2, stratify=y)
-
-mnb.fit(X_tr, y_tr)
-print("Validation accuracy:", mnb.score(X_val, y_val))
+    return cleaned_articles, author_dict
 
 
-
-print('\nLinearSVC')
-
-
-vectorizer = TfidfVectorizer(
-    analyzer='char',
-    ngram_range=(3,5),
-    max_features=5000
-)
-
-X_train = vectorizer.fit_transform(train_texts)
-X_test = vectorizer.transform(test_texts)
-
-model = LinearSVC(class_weight='balanced')
-model.fit(X_train, train_labels)
-
-predictions = model.predict(X_test)
-
-for i in range(len(test_ids)):
-    print(f"Paper {test_ids[i]} predicted author: {predictions[i]}")
-
-#Validation
-X = vectorizer.fit_transform(train_texts)
-y = train_labels
-
-X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size=0.2, stratify=y)
-
-mnb.fit(X_tr, y_tr)
-print("Validation accuracy:", mnb.score(X_val, y_val))
 
 '''
-\MultinomialNB
+MultinomialNB
 Paper 18 predicted author: MADISON
 Paper 19 predicted author: HAMILTON
 Paper 20 predicted author: HAMILTON
